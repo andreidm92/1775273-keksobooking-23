ee1e9f13363282838ecfc81e674f34d6eb61483b
Андрей <admi70@rambler.ru>
2021-06-01 15:24:23 +0300

    Исправил файл с Lint

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-let getIntRandom = function(min, max){
-    if (max - min >= 0) {
-        return randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
-    
-    }
-    return -1
-}
+const getIntRandom = function(min, max){
+  if (max - min >= 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return -1;
+};
 
-let getFloatRandom = function(min, max, digits){
-    if (max - min >= 0) {
-        let randomNumber = Math.random() * (max - min + 1) + min
-        return randomNumber.toFixed([digits])
-    
-    }
-    return -1
-}
-console.log(getFloatRandom(10, 15, 5))
+const getFloatRandom = function(min, max, digits){
+  if (max - min >= 0) {
+    const randomNumber = Math.random() * (max - min + 1) + min;
+    return randomNumber.toFixed([digits]);
+  }
+  return -1;
+};
+getFloatRandom(10, 15, 5);
+getIntRandom(10, 15);
