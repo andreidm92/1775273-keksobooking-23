e5a8baf2e869bcb6dc1cea95c896a44152939e78
Андрей <admi70@rambler.ru>
2021-06-24 00:46:30 +0300

    Домашняя работа 7

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {massiveObjects} from './data.js';
+import {generateAdvertising} from './generate.js';
 
+const mapCanvasElement = document.querySelector('.map__canvas');
+mapCanvasElement.appendChild(generateAdvertising(massiveObjects, 0));
 // eslint-disable-next-line no-console
-console.log(massiveObjects);
+console.log(generateAdvertising(massiveObjects, 0));
 
