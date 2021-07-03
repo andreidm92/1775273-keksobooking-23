1a7bce6515535407c721fee9fde375369d4e8ad7
Андрей <admi70@rambler.ru>
2021-07-03 11:00:17 +0300

    Дополнительная доработка ДЗ

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 import {massiveObjects} from './data.js';
 import {generateAdvertising} from './generate.js';
-//import {userNameInput} from './form.js';
-//import {roomNumber} from './form.js';
+import {userNameInput} from './form.js';
+import {roomNumber} from './form.js';
 
 const mapCanvasElement = document.querySelector('.map__canvas');
 mapCanvasElement.appendChild(generateAdvertising(massiveObjects, 1));
