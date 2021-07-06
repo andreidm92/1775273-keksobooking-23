de4046302eff880a8d7b57cccfeb9c3c3c00350c
Андрей <admi70@rambler.ru>
2021-07-06 20:34:04 +0300

    Не понятно зачем

--- js/main.js
+++ js/main.js
@@ -3,8 +3,26 @@ import {massiveObjects} from './data.js';
 import {generateAdvertising} from './generate.js';
 import {userNameInput} from './form.js';
 import {roomNumber} from './form.js';
+import {inactivePage, activePage} from './formMgt.js';
+
 
 const mapCanvasElement = document.querySelector('.map__canvas');
-mapCanvasElement.appendChild(generateAdvertising(massiveObjects, 1));
+//mapCanvasElement.appendChild(generateAdvertising(massiveObjects, 1));
+
+
+//inactivePage();
+activePage();
+
+fetch('https://23.javascript.pages.academy/keksobooking/data')
+  .then((response) => response.json())
+  .then((base) => {
+    console.log('Результат', base[1]);
+    console.log(generateAdvertising(base, 1));
+    for (let i=0; i < base.length; i++){
+      console.log(generateAdvertising(base, i));
+
+    }
+
+  });
 
 
